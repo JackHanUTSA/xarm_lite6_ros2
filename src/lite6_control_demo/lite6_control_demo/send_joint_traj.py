8b8edfa4037cad1b2b49ac1023f88f1b3d90fd11
Jack Han <kunlun1988@me.com>
2026-02-06 16:16:48 -0600

    Make send_joint_traj publish relative motion using /joint_states

--- src/lite6_control_demo/lite6_control_demo/send_joint_traj.py
+++ src/lite6_control_demo/lite6_control_demo/send_joint_traj.py
@@ -1,5 +1,6 @@
 import rclpy
 from rclpy.node import Node
+from sensor_msgs.msg import JointState
 from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
 
 
@@ -8,55 +9,64 @@ class SendJointTraj(Node):
         super().__init__('send_joint_traj')
 
         self.declare_parameter('topic', '/xarm6_traj_controller/joint_trajectory')
-        self.declare_parameter('joint_names', [
-            'joint1', 'joint2', 'joint3', 'joint4', 'joint5', 'joint6'
-        ])
+        self.declare_parameter('joint_names', ['joint1', 'joint2', 'joint3', 'joint4', 'joint5', 'joint6'])
         self.declare_parameter('delta_joint1', 0.10)  # radians
         self.declare_parameter('move_time', 8.0)      # seconds
+        self.declare_parameter('return_back', True)
 
-        topic = self.get_parameter('topic').value
+        self.topic = self.get_parameter('topic').value
         self.joint_names = list(self.get_parameter('joint_names').value)
         self.delta = float(self.get_parameter('delta_joint1').value)
         self.move_time = float(self.get_parameter('move_time').value)
+        self.return_back = bool(self.get_parameter('return_back').value)
 
-        self.pub = self.create_publisher(JointTrajectory, topic, 10)
-        self.get_logger().info(f'Publishing to {topic}')
-        self.get_logger().info(f'Joint names: {self.joint_names}')
-        self.get_logger().info(f'Delta joint1: {self.delta} rad, move_time: {self.move_time}s')
+        self.pub = self.create_publisher(JointTrajectory, self.topic, 10)
+        self.sub = self.create_subscription(JointState, '/joint_states', self._on_js, 10)
 
-        # one-shot timer
-        self.timer = self.create_timer(1.0, self._send_once)
-        self.sent = False
+        self._last_js = None
+        self._sent = False
 
-    def _send_once(self):
-        if self.sent:
+        self.get_logger().info(f'Publishing to {self.topic}')
+        self.get_logger().info('Waiting for /joint_states...')
+
+    def _on_js(self, msg: JointState):
+        self._last_js = msg
+        if self._sent:
             return
-        self.sent = True
 
-        msg = JointTrajectory()
-        msg.joint_names = self.joint_names
+        # Need all desired joints present
+        name_to_pos = {n: p for n, p in zip(msg.name, msg.position)}
+        if not all(n in name_to_pos for n in self.joint_names):
+            missing = [n for n in self.joint_names if n not in name_to_pos]
+            self.get_logger().warn(f'Missing joints in /joint_states: {missing}')
+            return
 
-        # We send a relative wiggle on joint1 by using two points:
-        # point1: all zeros at t=move_time
-        # point2: joint1 back to zero at t=2*move_time
-        # NOTE: If your controller expects absolute positions, this will move toward 0.
-        # Safer approach is to read current joint_states first; we can add that next.
+        current = [float(name_to_pos[n]) for n in self.joint_names]
+        target = current.copy()
+        target[0] = target[0] + self.delta
+
+        traj = JointTrajectory()
+        traj.joint_names = self.joint_names
 
         p1 = JointTrajectoryPoint()
-        p1.positions = [0.0] * len(self.joint_names)
-        p1.positions[0] = self.delta
+        p1.positions = target
         p1.time_from_start.sec = int(self.move_time)
         p1.time_from_start.nanosec = int((self.move_time - int(self.move_time)) * 1e9)
 
-        p2 = JointTrajectoryPoint()
-        p2.positions = [0.0] * len(self.joint_names)
-        p2.time_from_start.sec = int(2 * self.move_time)
-        p2.time_from_start.nanosec = int(((2 * self.move_time) - int(2 * self.move_time)) * 1e9)
+        traj.points = [p1]
 
-        msg.points = [p1, p2]
+        if self.return_back:
+            p2 = JointTrajectoryPoint()
+            p2.positions = current
+            t2 = 2.0 * self.move_time
+            p2.time_from_start.sec = int(t2)
+            p2.time_from_start.nanosec = int((t2 - int(t2)) * 1e9)
+            traj.points.append(p2)
 
-        self.pub.publish(msg)
-        self.get_logger().info('Trajectory published (2 points). Exiting in 2s...')
+        self.pub.publish(traj)
+        self._sent = True
+        self.get_logger().info(f'Sent relative trajectory: joint1 +{self.delta} rad (and back={self.return_back}).')
+        self.get_logger().info('Exiting in 2s...')
         self.create_timer(2.0, lambda: rclpy.shutdown())
 
 
